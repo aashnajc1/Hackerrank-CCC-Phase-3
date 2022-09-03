62ab37fd35f0447de14c4e766c99088889da7bb9
Aashna J Chandrashekhar <105263888+aashnajc1@users.noreply.github.com>
2022-09-03 15:44:58 +0530

    Add files via upload

---
+++ harry_potter_and_the_chamber_of_secrets.c
@@ -0,0 +1,96 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+typedef struct LinkedListNode LinkedListNode;
+
+struct LinkedListNode {
+    int val;
+    LinkedListNode *next;
+};
+
+LinkedListNode* _insert_node_into_singlylinkedlist(LinkedListNode *head, LinkedListNode *tail, int val) {
+    if(head == NULL) {
+        head = (LinkedListNode *) (malloc(sizeof(LinkedListNode)));
+        head->val = val;
+        head->next = NULL;
+        tail = head;
+    }
+    else {
+        LinkedListNode *node = (LinkedListNode *) (malloc(sizeof(LinkedListNode)));
+        node->val = val;
+        node->next = NULL;
+        tail->next = node;
+        tail = tail->next;
+    }
+    return tail;
+}
+
+
+/*
+ * Complete the function below.
+ */
+/*
+For your reference:
+LinkedListNode {
+    int val;
+    LinkedListNode *next;
+};
+*/
+bool hasCycle(LinkedListNode* head,int head_size) {
+  struct LinkedListNode *t = head;
+    int i=0;
+    while(t!=NULL){
+        i++;
+        if(i>head_size){
+            return true;
+            break;
+        }
+        t=t->next;
+    }
+    return false;
+    
+}
+
+int main()
+{
+    FILE *f = stdout;
+    char *output_path = getenv("OUTPUT_PATH");
+    if (output_path) {
+        f = fopen(output_path, "w");
+    }
+
+    bool res;
+    int head_size = 0;
+
+    LinkedListNode* head = NULL;
+    LinkedListNode* head_tail = NULL;
+
+    scanf("%d\n", &head_size);
+    for(int i = 0; i < head_size; i++) {
+        int head_item;
+        scanf("%d", &head_item);
+        head_tail = _insert_node_into_singlylinkedlist(head, head_tail, head_item);
+
+        if(i == 0) {
+            head = head_tail;
+        }
+    }
+	int x;
+  	scanf("%d", &x);
+  	if (x > -1)
+    {
+      	LinkedListNode* ptr = head;
+      	while (x--) ptr = ptr->next;
+      	head_tail->next = ptr;
+    }
+
+    res = hasCycle(head,head_size);
+    fprintf(f, "%d\n", res);
+
+    fclose(f);
+    return 0;
+}
